0dfb553fba9e56916aff53ed2187595457e2d5c7
kevinweber <k-iwi@web.de>
2014-08-23 04:20:24 +0200

    Switch toGMTString to toUTCString

--- js/wbounce.js
+++ js/wbounce.js
@@ -13,11 +13,13 @@ return function ouibounce(el, config) {
     aggressive   = config.aggressive || false,
     sensitivity  = setDefault(config.sensitivity, 20),
     timer        = setDefault(config.timer, 1000),
+    delay        = setDefault(config.delay, 0),
     callback     = config.callback || function() {},
     cookieExpire = setDefaultCookieExpire(config.cookieExpire) || '',
     cookieDomain = config.cookieDomain ? ';domain=' + config.cookieDomain : '',
     sitewide     = config.sitewide === true ? ';path=/' : '',
-    _html        = document.getElementsByTagName('html')[0];
+    _delayTimer  = null,
+    _html        = document.documentElement;
 
   function setDefault(_property, _default) {
     return typeof _property === 'undefined' ? _default : _property;
@@ -30,44 +32,57 @@ return function ouibounce(el, config) {
     var date = new Date();
     date.setTime(date.getTime() + ms);
 
-    return "; expires=" + date.toGMTString();
+    return "; expires=" + date.toUTCString();
   }
 
   setTimeout(attachOuiBounce, timer);
   function attachOuiBounce() {
     _html.addEventListener('mouseleave', handleMouseleave);
+    _html.addEventListener('mouseenter', handleMouseenter);
     _html.addEventListener('keydown', handleKeydown);
   }
 
   function handleMouseleave(e) {
     if (e.clientY > sensitivity || (checkCookieValue('viewedOuibounceModal', 'true') && !aggressive)) return;
-    fire();
-    callback();
+
+    _delayTimer = setTimeout(_fireAndCallback, delay);
+  }
+
+  function handleMouseenter(e) {
+    if (_delayTimer) {
+      clearTimeout(_delayTimer);
+      _delayTimer = null;
+    }
   }
 
   var disableKeydown = false;
   function handleKeydown(e) {
     if (disableKeydown || checkCookieValue('viewedOuibounceModal', 'true') && !aggressive) return;
-    else if(!e.metaKey || e.keyCode != 76) return;
+    else if(!e.metaKey || e.keyCode !== 76) return;
 
     disableKeydown = true;
-    fire();
-    callback();
+    _delayTimer = setTimeout(_fireAndCallback, delay);
   }
 
   function checkCookieValue(cookieName, value) {
+    return parseCookies()[cookieName] === value;
+  }
+
+  function parseCookies() {
     // cookies are separated by '; '
-    var cookies = document.cookie.split('; ').reduce(function(prev, curr) {
-      // split by '=' to get key, value pairs
-      var el = curr.split('=');
+    var cookies = document.cookie.split('; ');
+    
+    var ret = {};
+    for (var i = cookies.length - 1; i >= 0; i--) {
+      var el = cookies[i].split('=');
+      ret[el[0]] = el[1];
+    }
+    return ret;
+  }
 
-      // add the cookie to fn object
-      prev[el[0]] = el[1];
-
-      return prev;
-    }, {});
-
-    return cookies[cookieName] === value;
+  function _fireAndCallback() {
+    fire();
+    callback();
   }
 
   function fire() {
@@ -102,6 +117,7 @@ return function ouibounce(el, config) {
 
     // remove listeners
     _html.removeEventListener('mouseleave', handleMouseleave);
+    _html.removeEventListener('mouseenter', handleMouseenter);
     _html.removeEventListener('keydown', handleKeydown);
   }
 
